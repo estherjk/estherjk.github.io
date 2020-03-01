0c3662f52dc38d74114a730c8864ae4a23673b02
Esther Jun Kim <hello@esthermakes.tech>
2020-02-29 20:55:19 -0800

    Update image assets
    * Move brand assets to `assets/img/brand`
    * Move `background.jpg` to `assets/img`

--- scripts/mkfavicon.sh
+++ scripts/mkfavicon.sh
@@ -2,12 +2,12 @@
 
 # See http://bergamini.org/computers/creating-favicon.ico-icon-files-with-imagemagick-convert.html
 
-convert assets/brand-512.png -resize 16x16 favicon-16.png
-convert assets/brand-512.png -resize 32x32 favicon-32.png
-convert assets/brand-512.png -resize 64x64 favicon-64.png
-convert assets/brand-512.png -resize 128x128 favicon-128.png
-convert assets/brand-512.png -resize 152x152 favicon-152.png
-convert assets/brand-512.png -resize 256x256 favicon-256.png
+convert assets/img/brand-512.png -resize 16x16 favicon-16.png
+convert assets/img/brand-512.png -resize 32x32 favicon-32.png
+convert assets/img/brand-512.png -resize 64x64 favicon-64.png
+convert assets/img/brand-512.png -resize 128x128 favicon-128.png
+convert assets/img/brand-512.png -resize 152x152 favicon-152.png
+convert assets/img/brand-512.png -resize 256x256 favicon-256.png
 convert favicon-16.png favicon-32.png favicon-64.png favicon-128.png favicon-256.png -colors 256 favicon.ico
 
 mv favicon.ico assets
